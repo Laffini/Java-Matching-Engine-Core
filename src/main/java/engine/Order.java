c9310b7d01eae9a53e57e9f123c43d006cfe2f22
Tom Lafford <tom@laffyco.net>
2020-12-16 18:57:34 +0000

    Restructure & adding limit buy/sell.

--- src/main/java/engine/Order.java
+++ src/main/java/engine/Order.java
@@ -6,62 +6,39 @@ package engine;
  */
 public class Order {
 
-    private int orderId;
-    private String instrument;
+    private int amount;
     private double price;
-    private double quantity;
-    private double cumulativeQuantity;
-    private double leavesQuantity;
+    private String id;
     private Side side;
 
     /**
-     * Create a new instance of Order
-     *
-     * @param orderId
-     * @param instrument
+     * Create an instance of Order.
+     * 
+     * @param amount
      * @param price
-     * @param quantity
-     * @param cumulativeQuantity
-     * @param leavesQuantity
+     * @param id
      * @param side
      */
-    public Order(final int orderId, final String instrument, final double price, final double quantity,
-            final double cumulativeQuantity, final double leavesQuantity, final Side side) {
-        this.orderId = orderId;
-        this.instrument = instrument;
+    public Order(final int amount, final double price, final String id, final Side side) {
+        super();
+        this.amount = amount;
         this.price = price;
-        this.quantity = quantity;
-        this.cumulativeQuantity = cumulativeQuantity;
-        this.leavesQuantity = leavesQuantity;
+        this.id = id;
         this.side = side;
     }
 
     /**
-     * @return the orderId
+     * @return the amount
      */
-    public int getOrderId() {
-        return this.orderId;
+    public int getAmount() {
+        return this.amount;
     }
 
     /**
-     * @param orderId the orderId to set
+     * @param amount the amount to set
      */
-    public void setOrderId(final int orderId) {
-        this.orderId = orderId;
-    }
-
-    /**
-     * @return the instrument
-     */
-    public String getInstrument() {
-        return this.instrument;
-    }
-
-    /**
-     * @param instrument the instrument to set
-     */
-    public void setInstrument(final String instrument) {
-        this.instrument = instrument;
+    public void setAmount(final int amount) {
+        this.amount = amount;
     }
 
     /**
@@ -79,45 +56,17 @@ public class Order {
     }
 
     /**
-     * @return the quantity
+     * @return the id
      */
-    public double getQuantity() {
-        return this.quantity;
+    public String getId() {
+        return this.id;
     }
 
     /**
-     * @param quantity the quantity to set
+     * @param id the id to set
      */
-    public void setQuantity(final double quantity) {
-        this.quantity = quantity;
-    }
-
-    /**
-     * @return the cumulativeQuantity
-     */
-    public double getCumulativeQuantity() {
-        return this.cumulativeQuantity;
-    }
-
-    /**
-     * @param cumulativeQuantity the cumulativeQuantity to set
-     */
-    public void setCumulativeQuantity(final double cumulativeQuantity) {
-        this.cumulativeQuantity = cumulativeQuantity;
-    }
-
-    /**
-     * @return the leavesQuantity
-     */
-    public double getLeavesQuantity() {
-        return this.leavesQuantity;
-    }
-
-    /**
-     * @param leavesQuantity the leavesQuantity to set
-     */
-    public void setLeavesQuantity(final double leavesQuantity) {
-        this.leavesQuantity = leavesQuantity;
+    public void setId(final String id) {
+        this.id = id;
     }
 
     /**
--- src/main/java/engine/OrderBook.java
+++ src/main/java/engine/OrderBook.java
@@ -1,6 +1,6 @@
 package engine;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 /**
  * @author Laffini
@@ -8,45 +8,243 @@ import java.util.HashMap;
  */
 public class OrderBook {
 
-    private final HashMap<String, String> bids;
-    private final HashMap<String, String> asks;
-    private final HashMap<String, String> orderIdMap;
+    private ArrayList<Order> buyOrders;
+    private ArrayList<Order> sellOrders;
 
     /**
-     * Create an instance of OrderBook
+     * Create an instance of OrderBook.
      *
-     * @param bids
-     * @param asks
-     * @param orderIdMap
+     * @param buyOrders
+     * @param sellOrders
      */
-    public OrderBook() {
-        this.bids = new HashMap<String, String>(); // TODO: Change to used datatype
+    public OrderBook(final ArrayList<Order> buyOrders, final ArrayList<Order> sellOrders) {
+        super();
+        this.buyOrders = buyOrders;
+        this.sellOrders = sellOrders;
+    }
 
-        this.asks = new HashMap<String, String>(); // TODO: Change to used datatype
+    /**
+     * Process an order and return the trades generated before adding the remaining
+     * amount to the market.
+     *
+     * @param Order
+     */
+    public synchronized ArrayList<Trade> process(final Order order) {
 
-        this.orderIdMap = new HashMap<String, String>(); // TODO: Change to used datatype
+        if (order.getSide() == Side.BUY) {
+            return this.processLimitBuy(order);
+        } else {
+            return this.processLimitSell(order);
+        }
 
     }
 
     /**
-     * @return the bids
+     * Process limit buy.
+     *
+     * @param order
+     * @return Trades.
      */
-    public synchronized HashMap<String, String> getBids() {
-        return this.bids;
+    public synchronized ArrayList<Trade> processLimitBuy(final Order order) {
+        final ArrayList<Trade> trades = new ArrayList<Trade>();
+
+        final int n = this.sellOrders.size();
+
+        // Check if at least one matching order.
+        if (n != 0 || this.sellOrders.get(n - 1).getPrice() <= order.getPrice()) {
+
+            // Traverse matching orders
+            for (int i = n - 1; i >= 0; i++) {
+                final Order sellOrder = this.sellOrders.get(i);
+                if (sellOrder.getPrice() > order.getPrice()) {
+                    break;
+                }
+                // Fill entire order.
+                if (sellOrder.getAmount() >= order.getAmount()) {
+                    trades.add(new Trade(order.getId(), sellOrder.getId(), order.getAmount(), sellOrder.getPrice()));
+                    sellOrder.setAmount(sellOrder.getAmount() - order.getAmount());
+                    if (sellOrder.getAmount() == 0) {
+                        this.removeSellOrder(i);
+                    }
+                    return trades;
+                }
+
+                // Fill partial order & continue.
+                if (sellOrder.getAmount() < order.getAmount()) {
+                    trades.add(
+                            new Trade(order.getId(), sellOrder.getId(), sellOrder.getAmount(), sellOrder.getPrice()));
+                    order.setAmount(order.getAmount() - sellOrder.getAmount());
+                    this.removeSellOrder(i);
+                    continue;
+                }
+
+            }
+
+        }
+
+        // Add remaining order to book.
+        this.addBuyOrder(order);
+
+        return trades;
     }
 
     /**
-     * @return the asks
+     * Process a limit sell.
+     * 
+     * @param order
+     * @return Trades.
      */
-    public synchronized HashMap<String, String> getAsks() {
-        return this.asks;
+    public synchronized ArrayList<Trade> processLimitSell(final Order order) {
+        final ArrayList<Trade> trades = new ArrayList<Trade>();
+
+        final int n = this.buyOrders.size();
+
+        // Check that there is at least one matching order.
+        if (n != 0 || this.buyOrders.get(n - 1).getPrice() >= order.getPrice()) {
+            // Traverse all matching orders.
+            for (int i = n - 1; i >= 0; i++) {
+                final Order buyOrder = this.buyOrders.get(i);
+                if (buyOrder.getPrice() < order.getPrice()) {
+                    break;
+                }
+
+                // Fill entire order.
+                if (buyOrder.getAmount() >= order.getAmount()) {
+                    trades.add(new Trade(order.getId(), buyOrder.getId(), order.getAmount(), buyOrder.getPrice()));
+                    buyOrder.setAmount(buyOrder.getAmount() - order.getAmount());
+                    if (buyOrder.getAmount() == 0) {
+                        this.removeBuyOrder(i);
+                    }
+                    return trades;
+                }
+
+                // Fill partial order and continue.
+                if (buyOrder.getAmount() < order.getAmount()) {
+                    trades.add(new Trade(order.getId(), buyOrder.getId(), buyOrder.getAmount(), buyOrder.getPrice()));
+                    order.setAmount(order.getAmount() - buyOrder.getAmount());
+                    this.removeBuyOrder(i);
+                    continue;
+                }
+
+            }
+
+        }
+        // Add remaining order to the list.
+        this.addSellOrder(order);
+        return trades;
+
     }
 
     /**
-     * @return the orderIdMap
+     * Add a buy order to the order book.
+     *
+     * @param order
      */
-    public synchronized HashMap<String, String> getOrderIdMap() {
-        return this.orderIdMap;
+    public synchronized void addBuyOrder(final Order order) {
+
+        final int n = this.buyOrders.size();
+
+        int i;
+
+        for (i = n - 1; i >= 0; i--) {
+
+            final Order buyOrder = this.buyOrders.get(i);
+
+            if (buyOrder.getPrice() < order.getPrice()) {
+                break;
+            }
+
+        }
+        if (i == n - 1) {
+            this.buyOrders.add(order);
+        } else {
+
+            final Order current = this.buyOrders.get(i);
+
+            this.buyOrders.set(i + 1, current);
+
+            this.buyOrders.set(i, order);
+
+        }
+
+    }
+
+    /**
+     * Add a sell order to the order book.
+     *
+     * @param order
+     */
+    public synchronized void addSellOrder(final Order order) {
+
+        final int n = this.sellOrders.size();
+
+        int i;
+
+        for (i = n - 1; i >= 0; i++) {
+
+            final Order sellOrder = this.sellOrders.get(i);
+
+            if (sellOrder.getPrice() > order.getPrice()) {
+                break;
+            }
+        }
+
+        if (i == n - 1) {
+            this.sellOrders.add(order);
+        } else {
+            final Order current = this.buyOrders.get(i);
+
+            this.buyOrders.set(i + 1, current);
+
+            this.buyOrders.set(i, order);
+        }
+
+    }
+
+    /**
+     * Remove a buy order from the order book.
+     *
+     * @param index
+     */
+    public synchronized void removeBuyOrder(final int index) {
+        this.buyOrders.remove(index);
+    }
+
+    /**
+     * Remove a sell order from the order book.
+     *
+     * @param index
+     */
+    public synchronized void removeSellOrder(final int index) {
+        this.sellOrders.remove(index);
+    }
+
+    /**
+     * @return the buyOrders
+     */
+    public synchronized ArrayList<Order> getBuyOrders() {
+        return this.buyOrders;
+    }
+
+    /**
+     * @param buyOrders the buyOrders to set
+     */
+    public synchronized void setBuyOrders(final ArrayList<Order> buyOrders) {
+        this.buyOrders = buyOrders;
+    }
+
+    /**
+     * @return the sellOrders
+     */
+    public synchronized ArrayList<Order> getSellOrders() {
+        return this.sellOrders;
+    }
+
+    /**
+     * @param sellOrders the sellOrders to set
+     */
+    public synchronized void setSellOrders(final ArrayList<Order> sellOrders) {
+        this.sellOrders = sellOrders;
     }
 
 }
